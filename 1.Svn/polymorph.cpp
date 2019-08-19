83995e0eaa10c1eb6dedd2be58a1605f8922a8ef
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-08-19 20:52:17 +0300

    Add files via upload

--- 1.Svn/polymorph.cpp
+++ 1.Svn/polymorph.cpp
@@ -3,12 +3,5 @@ bool CPolymorphUtils::CheckMap(LPCHARACTER pChar)
 {
 	std::list<DWORD> maplist {113, 41}; // edit
 	const auto it = std::find(maplist.begin(), maplist.end(), pChar->GetMapIndex());
-	if (it != maplist.end()) {
-		if (pChar->IsPolymorphed()){
-			pChar->RemoveAffect(AFFECT_POLYMORPH);
-			pChar->SetPolymorph(0);
-		}
-		return false;
-	}
-	return true;
+	return it == maplist.end();
 }
